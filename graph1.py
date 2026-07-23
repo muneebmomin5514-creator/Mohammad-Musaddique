9d1cd4d0ba75fc9d9ee7174452fd34fcf48f0d22
muneebmomin5514-creator <muneebmomin5514@gmail.com>
2026-07-23 07:58:59 +0530

    Create graph1.py
    My first graph

---
+++ graph1.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+x=[1,2,3,4,5,6,7,8,9,10,11,12]
+y=[23,45,56,76,89,43,12,44,65,87,90,54]
+plt.plot(x,y)
+
+plt.title("monthly sales")
+plt.xlabel("months")
+plt.ylabel("sales")
+
+
+plt.grid()
+plt.show()
